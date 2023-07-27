f5422ab9f845c7a1923d12b8a87072451083fcab
Andres Biasutto <aquiandresbiasutto@gmail.com>
2023-07-27 04:00:35 -0300

    back ready to deploy

--- api/index.js
+++ api/index.js
@@ -19,10 +19,13 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const {
+  DB_PORT
+} = process.env;
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(DB_PORT || 3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
